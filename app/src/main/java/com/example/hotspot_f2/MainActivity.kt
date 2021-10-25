e404d8f9e9ddc451cb593d03c757c11d4619644b
Markus Thieden <mit@patentrenewal.com>
2021-10-25 11:59:51 +0200

    temp version while making navbar

--- app/src/main/java/com/example/hotspot_f2/MainActivity.kt
+++ app/src/main/java/com/example/hotspot_f2/MainActivity.kt
@@ -1,16 +1,42 @@
 package com.example.hotspot_f2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            TopBar()
             Text("sut min dut")
+            }
         }
 
     }
+
+
+
+
+@Composable
+fun TopBar() {
+    TopAppBar(
+        title = { Text(text = stringResource(R.string.app_name), fontSize = 18.sp) },
+        backgroundColor = colorResource(id = R.color.colorPrimary),
+        contentColor = Color.White
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopBarPreview() {
+    TopBar()
 }
