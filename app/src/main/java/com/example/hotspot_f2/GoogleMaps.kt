5f1b595a6b860f76644d303a0b2fe4ea6ea33af5
y62 <y62@outlook.dk>
2022-01-05 01:28:18 +0100

    multiple locations

--- app/src/main/java/com/example/hotspot_f2/GoogleMaps.kt
+++ app/src/main/java/com/example/hotspot_f2/GoogleMaps.kt
@@ -16,16 +16,14 @@ import androidx.compose.ui.Modifier
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.CameraPosition
-
-
-
+import com.google.android.gms.maps.model.MarkerOptions
+import java.util.ArrayList
 
 
 @Composable
 fun Hotspotmap(
     modifier:Modifier=Modifier,
     OnReady:(GoogleMap)->Unit
-
 )
 {
     val context= LocalContext.current
@@ -37,27 +35,71 @@ fun Hotspotmap(
 
     val mapView= remember {
         MapView(context)
-
     }
 
     val lifecycle= LocalLifecycleOwner.current.lifecycle
 
-    lifecycle.addObserver(rememberMapLifecycle(mapView ))
+    lifecycle.addObserver(rememberMapLifecycle(mapView))
+
+    val destination1 = LatLng(55.7215, 12.3639)
+    val destination2 = LatLng(55.7215, 12.3639)
+    val destination3 = LatLng(55.7142, 12.3904)
+    val destination4 = LatLng(55.7165, 12.4185)
+    val locationArrayList: MutableList<LatLng> = ArrayList()
+    locationArrayList.add(destination1);
+    locationArrayList.add(destination2);
+    locationArrayList.add(destination3);
+    locationArrayList.add(destination4);
+
 
     AndroidView(
         factory = {
-                  mapView.apply {
-                      mapView.getMapAsync { googleMap->
-                          googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
-                          OnReady(googleMap)
+            mapView.apply {
+                mapView.getMapAsync { googleMap->
+                    googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+                    OnReady(googleMap)
+                    for (i in locationArrayList.indices) {
+                        val markerOptionsDestination = MarkerOptions()
+                            .title("HOTSPOT")
+                            .snippet("Here you can drink free vodka. No cap")
+                            .position(locationArrayList[i])
+                        googleMap.addMarker(markerOptionsDestination)
 
-                      }
-                  }
+                    }
+                }
+            }
         },
         modifier=modifier
     )
 
+
+
+  /*  AndroidView(
+        factory = {
+            mapView.apply {
+                mapView.getMapAsync { googleMap->
+                    googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+                    OnReady(googleMap)
+                    val melbourneLatLng = LatLng(55.7215, 12.3639)
+                    val melbourne = googleMap.addMarker(
+                        MarkerOptions()
+                            .position(melbourneLatLng)
+                            .title("Ballerup")
+                            .snippet("Population: 4,137,400")
+                    )
+                    melbourne?.showInfoWindow()
+
+
+                }
+            }
+        },
+        modifier=modifier
+    )
+*/
+
 }
+
+
 @Composable
 fun rememberMapLifecycle(map:MapView):LifecycleObserver{
     return remember{
