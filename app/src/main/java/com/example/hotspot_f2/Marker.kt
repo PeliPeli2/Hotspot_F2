bcf3ac61953e2995233119b2e187eedbc95ffef8
s164270 <s164270@student.dtu.dk>
2022-01-06 10:33:50 +0100

    buttons for hiding/showing the input fields

--- app/src/main/java/com/example/hotspot_f2/Marker.kt
+++ app/src/main/java/com/example/hotspot_f2/Marker.kt
@@ -1,8 +1,10 @@
 package com.example.hotspot_f2
 
 import android.content.Context
+import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -19,19 +21,46 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun AddMarker (
+fun AddMarker(
     context: Context = LocalContext.current,
     modifier: Modifier = Modifier
 ) {
 
+    val showInputForm = remember { mutableStateOf(false) }
 
     val textState1 = remember { mutableStateOf(TextFieldValue()) }
     val textState2 = remember { mutableStateOf(TextFieldValue()) }
     val textState3 = remember { mutableStateOf(TextFieldValue()) }
     val textState4 = remember { mutableStateOf(TextFieldValue()) }
 
-        Column(Modifier.padding(16.dp)) {
-//  Text("The text: " + textState1.value.text)
+
+
+    Column(Modifier.padding(16.dp)) {
+        if (showInputForm.value) {
+            Row() {
+                // Cancel adding a hotspot
+                Button(modifier = Modifier.padding(end = 4.dp), onClick = { showInputForm.value = false }) {
+                    Text("Cancel")
+                }
+
+                Button(onClick = {
+                    showInputForm.value = true //hide after adding hotspot
+
+                    Log.d("???", "ASDJKLASJDKLJASD")
+
+                    //function
+                    val newVar = textState1.value.text
+                    Toast.makeText(context, newVar.toString(), Toast.LENGTH_SHORT).show()
+                    fun testFunction(): String {
+                        return newVar
+                    }
+                }
+                ) {
+                    Text("Add")
+                }
+            }
+
+            //  Text("The text: " + textState1.value.text)
             TextField(
                 value = textState1.value,
                 textStyle = TextStyle(color = Color.Red, fontWeight = FontWeight.Bold),
@@ -61,21 +90,11 @@ fun AddMarker (
             )
 
         }
-                    Button(onClick = {
-
-                //function
-                val newVar = textState1.value.text
-                        Toast.makeText(context, newVar.toString(), Toast.LENGTH_SHORT).show()
-                        fun testFunction(): String{
-                            return newVar
-                        }
+        else {
+            // show input form when button is clicked
+            Button(onClick = { showInputForm.value = true }) {
+                Text("Add Hotspot")
+            }
+        }
     }
-
-
-    ) {
-        Text("Add Hotspot")
-    }
-
-
-
 }
