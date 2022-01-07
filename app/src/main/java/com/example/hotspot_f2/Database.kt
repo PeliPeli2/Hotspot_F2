ba35d3e68805372bb157fc76bcd6c0009544c275
PeliPeli2 <harisinavi@gmail.com>
2022-01-07 09:40:37 +0100

    Get hotspots from Database

--- app/src/main/java/com/example/hotspot_f2/Database.kt
+++ app/src/main/java/com/example/hotspot_f2/Database.kt
@@ -1,21 +1,25 @@
 package com.example.hotspot_f2
 
 import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.firestore.GeoPoint
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import java.util.zip.CheckedInputStream
 
 class Database() {
     private companion object {
         private const val TAG = "FIRESTOREDB"
     }
-
+@Composable
     fun getHotspots(): List<HotspotData> {
         return getDummyHotspots() //TODO: get hotspots from Firestore database
     }
-
+@Composable
     private fun getDummyHotspots(): List<HotspotData> {
 
         val point1 = HotspotData(
@@ -24,34 +28,35 @@ class Database() {
                     " historic retreat—the perfect post-sightseeing reprieve. Specializing in Champagne," +
                     " along with a well-edited list of wine and cocktails and late-afternoon live jazz performances.",
             location = GeoPoint(55.73,12.3962),
-            connections = 3)
+            "Kongens Nytorv",
+            connections = 3, painterResource(id = R.drawable.broennum),"Bar",10 )
 
         val point2 = HotspotData(
             title="Duck And Cover",
             description = "Duck and Cover is a subterranean sanctuary in bustling Vesterbro. " +
                     "In contrast to the neighborhood’s other packed-to-the-gills bars, it's a relaxed, " +
                     "ideal place to pop in for a subdued nightcap amid a striking collection of vintage furniture and lighting.",
-            location = GeoPoint(55.73,12.39),
-            connections = 7)
+            location = GeoPoint(55.73,12.39),"Vesterbro",
+            connections = 7, painterResource(id = R.drawable.duck_and_cover),"Bar",5 )
 
         val point3 = HotspotData(
             title="Ørsted",
             description = "Copenhagen's craft beer scene is impressive, and one of the best places to become acquainted with it is Ørsted Ølbar," +
                     " located a stone’s throw from Nørreport Station and tranquil Ørstedparken.",
-            location = GeoPoint(55.7250,12.3910),
-            connections = 12)
+            location = GeoPoint(55.7250,12.3910), "København K",
+            connections = 12,painterResource(id = R.drawable.oersted),"Bar",30 )
 
         val point4 = HotspotData(
             title="K-bar",
             description = "K-bar is named for Kirsten Holm, " +
                     "a pioneer of the Copenhagen cocktail scene. Years after opening, this bar, " +
                     "situated near Højbro Square, remains a sought-after drinks destination",
-            location = GeoPoint(55.7279,12.39),
-            connections = 34)
+            location = GeoPoint(55.7279,12.39), "København K",
+            connections = 34,painterResource(id = R.drawable.bar),"Bar",40 )
 
         return listOf(point1, point2, point3, point4)
     }
-
+@Composable
     fun writeDummyHotspots() {
         getDummyHotspots().forEach { writeHotspot(it) }
     }
@@ -101,5 +106,9 @@ data class HotspotData(
     val title: String,
     val description: String,
     val location: GeoPoint,
-    val connections: Int)
+    val locationname: String,
+    val connections: Int,
+    val image: Painter,
+    val type: String,
+    val checkedins: Int)
 
