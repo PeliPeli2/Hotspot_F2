d07f04d49206c8165ee8cc48133dd5e79c9b1be4
Markus Thieden <mit@patentrenewal.com>
2022-01-10 13:58:29 +0100

    firebase implementation of profileScreen

--- app/src/main/java/com/example/hotspot_f2/ui/ProfileScreen.kt
+++ app/src/main/java/com/example/hotspot_f2/ui/ProfileScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
@@ -22,6 +23,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -29,7 +32,6 @@ import com.example.hotspot_f2.Database
 import com.example.hotspot_f2.MainActivity
 import com.example.hotspot_f2.ProfileViewModel
 import com.google.firebase.auth.FirebaseAuth
-
 
 
 @Composable
@@ -45,32 +47,6 @@ fun ProfileScreen(profileViewModel: ProfileViewModel) {
     }
 }
 
-@Composable
-fun TopButtons(profileViewModel: ProfileViewModel) {
-    val context = LocalContext.current
-    var edit by rememberSaveable { mutableStateOf(false) }
-    var editText by rememberSaveable { mutableStateOf("Edit profile") }
-    Row() {
-        Button(onClick = {
-            val firebaseAuth = FirebaseAuth.getInstance()
-            firebaseAuth.signOut()
-            context.startActivity(Intent(context, MainActivity::class.java))
-        })
-        { Text(text = "Logout") }
-        Button(onClick = {
-            if(edit)
-                editText= "Done"
-            else
-                editText= "Edit profile"
-            edit =!edit
-        }) { Text(text = editText) }
-        Button(onClick = { Database().testGetUser("Adam Abel", profileViewModel) }) { Text(text = "Adam") }
-        Button(onClick = { Database().getCurrentUser( profileViewModel) }) { Text(text = "Current") }
-        Button(onClick = { Database().updateCurrentUser(profileViewModel) }) { Text(text = "Write current to DB") }
-    }
-}
-
-
 
 @Composable
 fun ProfileSection(
@@ -82,7 +58,28 @@ fun ProfileSection(
     var age by rememberSaveable { profileViewModel.age }
     var description by rememberSaveable { profileViewModel.description }
 
-    TopButtons(profileViewModel)
+    val context = LocalContext.current
+    var edit by rememberSaveable { mutableStateOf(false) }
+    var editText by rememberSaveable { mutableStateOf("Edit profile") }
+    Row() {
+        Button(onClick = {
+            val firebaseAuth = FirebaseAuth.getInstance()
+            firebaseAuth.signOut()
+            context.startActivity(Intent(context, MainActivity::class.java))
+        })
+        { Text(text = "Logout") }
+        Button(onClick = {
+            if(!edit)
+                editText= "Done"
+            else {
+                Database().updateCurrentUser(profileViewModel)
+                editText = "Edit profile"
+            }
+                edit =!edit
+        }) { Text(text = editText) }
+        Button(onClick = { Database().testGetUser("Adam Abel", profileViewModel) }) { Text(text = "Adam") }
+        Button(onClick = { Database().getCurrentUser( profileViewModel) }) { Text(text = "Current") }
+    }
     Column(
         modifier = modifier
             .fillMaxWidth()
@@ -107,21 +104,41 @@ fun ProfileSection(
                 .fillMaxWidth()
                 .padding(horizontal = 20.dp)
         ) {
-            Spacer(modifier = Modifier.width(16.dp))
-            StatSection(name, age.toString(), modifier = Modifier.weight(7f))
-            // LogoutButton(profileViewModel)
-
+            if (!edit){
+                Spacer(modifier = Modifier.width(16.dp))
+                StatSection(name, age.toString(), modifier = Modifier.weight(7f))
+            }
+            else{
+                OutlinedTextField(
+                    value = name,
+                    onValueChange = { name = it },
+                    label = { Text("Name") }
+                )
+                OutlinedTextField(
+                    value = age.toString(),
+                    onValueChange = {
+                    if(it.toIntOrNull()!=null)
+                        age = it.toInt() },
+                    label = { Text("Age") },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number,
+                        imeAction = ImeAction.Next),
+                )
+            }
         }
-        ProfileDescription(
-            description = description,
-        )
-
-        OutlinedTextField(
-            value = name,
-            onValueChange = { name = it },
-            label = { Text("Name") }
-        )
-
+        if (!edit) {
+            ProfileDescription(
+                description = description,
+            )
+        }
+        else
+        {
+            OutlinedTextField(
+                value = description,
+                onValueChange = { description = it },
+                label = { Text("Description") }
+            )
+        }
     }
 }
 
